212b03a7ff56a4a7dd1ae6483d32c92f60035f9a
Suneel Pirkash <f190969@nu.edu.pk>
2026-09-24 10:25:11 +0500

    chore: add Google Tag Manager (GTM-56ZJLWX2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -132,7 +132,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 
   return (
     <html lang="en-US" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-56ZJLWX2');` }} />
+      </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56ZJLWX2" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
 
         {isRealGa && (
